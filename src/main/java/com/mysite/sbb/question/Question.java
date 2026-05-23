92f2838e97fdecbb860ca951aa0aedcb7a77ded6
oxcm07 <oxcm0727@gmail.com>
2026-05-23 16:45:07 +0900

    2-12

--- src/main/java/com/mysite/sbb/question/Question.java
+++ src/main/java/com/mysite/sbb/question/Question.java
@@ -6,6 +6,7 @@ import lombok.Getter;
 import lombok.Setter;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
 import java.util.List;
 
 @Getter
@@ -28,6 +29,18 @@ public class Question {
     private List<Answer> answerList;
 
     public void setCreateDate(LocalDateTime now) {
+    }
 
+    @OneToMany(mappedBy = "question", cascade = {CascadeType.PERSIST, CascadeType.REMOVE})
+    private List<Answer> answers = new ArrayList<>();
+
+    public Answer addAnswer(String content) {
+        Answer answer = new Answer();
+        answer.setContent(content);
+        answer.setQuestion(this);
+        answer.setCreateDate(LocalDateTime.now());
+        answers.add(answer);
+
+        return answer;
     }
 }
